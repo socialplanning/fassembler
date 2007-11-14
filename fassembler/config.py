0ae3660f9d726c9006c370f388c9ef17a9c8033f
Ian Bicking <ianb@colorstudy.com>
2007-11-14 16:54:54 +0000

    Add a getdefault option

--- fassembler/config.py
+++ fassembler/config.py
@@ -5,3 +5,9 @@ class ConfigParser(configparser.RawConfigParser):
     inherit_defaults = False
     extendable = True
     default_extend_section_name = '__name__'
+
+    def getdefault(self, section, option, default=None):
+        try:
+            return self.get(section, option)
+        except (NoSectionError, NoOptionError), e:
+            return default
