ec13ec0fd80a20f66334802deb13924812a1bfc9
Ian Bicking <ianb@colorstudy.com>
2007-11-29 22:58:56 +0000

    Added a bundle checkout and symlink

--- fassembler/topp_opencore.py
+++ fassembler/topp_opencore.py
@@ -10,6 +10,7 @@ from fassembler.project import Project, Setting
 from fassembler import tasks
 interpolated = tasks.interpolated
 import warnings
+from glob import glob
 
 warnings.filterwarnings('ignore', 'tempnam is .*')
 
@@ -21,6 +22,14 @@ class InstallZope(tasks.Task):
 
     dest_path = interpolated('src_path')
     version_path = interpolated('version_path')
+    _tarball_url = tarball_url
+    _orig_zope_source = orig_zope_source
+
+    description = """
+    Install Zope into {{task.dest_path}}.
+
+    This downloads Zope from {{task._tarball_url}}, which was itself built from {{task._orig_zope_source}}
+    """
 
     def __init__(self, stacklevel=1):
         super(InstallZope, self).__init__(
@@ -97,6 +106,47 @@ def make_tarball():
     print 'You may want to run this now:'
     print '  scp %s flow.openplans.org:/www/svn.openplans.org/eggs/' % os.path.join(dir, filename)
 
+class SymlinkProducts(tasks.Task):
+
+    source_glob = interpolated('source_glob')
+    dest_dir = interpolated('dest_dir')
+    exclude_glob = interpolated('exclude_glob')
+
+    description = """
+    Symlink the files {{task.source_glob}} ({{len(task.source_files)}} files and directories total)
+    to {{task.dest_dir}}
+    {{if task.exclude_glob}}
+    Also exclude any files matching {{task.exclude_glob}} ({{task.exclude_count}} files and directories excluded)
+    {{endif}}
+    """
+
+    def __init__(self, name, source_glob, dest_dir, exclude_glob=None, stacklevel=1):
+        super(SymlinkProducts, self).__init__(name, stacklevel=stacklevel+1)
+        self.source_glob = source_glob
+        self.dest_dir = dest_dir
+        self.exclude_glob = exclude_glob
+
+    @property
+    def source_files(self):
+        results  =[]
+        if self.exclude_glob:
+            exclude = glob(self.exclude_glob)
+        else:
+            exclude = []
+        for filename in glob(self.source_glob):
+            if filename not in exclude:
+                results.append(filename)
+        return results
+
+    @property
+    def exclude_count(self):
+        return len(glob(self.exclude_glob))
+
+    def run(self):
+        for filename in self.source_files:
+            dest = os.path.join(self.dest_dir, os.path.basename(filename))
+            self.maker.ensure_symlink(filename, dest)
+
 class OpenCoreProject(Project):
     """
     Install OpenCore
@@ -157,6 +207,12 @@ class OpenCoreProject(Project):
         Setting('email_confirmation',
                 default='0',
                 help='Whether to send email configuration'),
+        ## FIXME: this could differ for different profiles
+        ## e.g., there's another bundle at:
+        ##   https://svn.openplans.org/svn/deployment/products-plone25
+        Setting('opencore_bundle_repo',
+                default='https://svn.openplans.org/svn/bundles/opencore-plone25',
+                help='The location of the svn bundle which contains all our products'),
         ]
 
     files_dir = os.path.join(os.path.dirname(__file__), 'opencore-files')
@@ -168,7 +224,7 @@ class OpenCoreProject(Project):
     start_script_template = """\
 #!/bin/sh
 cd {{env.base_path}}
-exec {{env.base_path}}/var/opencore/zope/bin/zopectl fg
+exec {{env.base_path}}/var/opencore/zope/bin/runzope -X debug-mode=off
 """
 
     actions = [
@@ -197,8 +253,14 @@ exec {{env.base_path}}/var/opencore/zope/bin/zopectl fg
         tasks.Script('Make ZEO Instance', [
         'python', '{{config.zope_source}}/bin/mkzeoinstance.py', '{{config.zeo_instance}}', '{{config.zeo_port}}'],
                      use_virtualenv=True),
+        tasks.SvnCheckout('Check out bundle',
+                          '{{config.opencore_bundle_repo}}',
+                          '{{env.base_path}}/opencore/src/opencore-bundle'),
+        SymlinkProducts('Symlink Products',
+                        '{{env.base_path}}/opencore/src/opencore-bundle/*',
+                        '{{config.zope_instance}}/Products',
+                        exclude_glob='{{env.base_path}}/opencore/src/opencore-bundle/ClockServer'),
         ## FIXME: linkzope and linkzopebinaries?
-        ## FIXME: Write start script
         tasks.InstallSupervisorConfig(),
         tasks.EnsureFile('Write the start script',
                          '{{env.base_path}}/bin/start-{{project.name}}',
