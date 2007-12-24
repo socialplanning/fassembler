4f778a758d0dcf6b536a02d0c3046b3c4f4ba329
Douglas Mayle <douglas@mayle.org>
2007-12-24 20:21:00 +0000

    Make that a more generic sourceforge source page

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(name='fassembler',
       ],
       ## FIXME: release all of these once fassembler stabilizes:
       dependency_links=[
-          'http://internap.dl.sourceforge.net/sourceforge/mysql-python/MySQL-python-1.2.2.tar.gz',
+          'http://sourceforge.net/project/showfiles.php?group_id=22307&package_id=15775&release_id=491012',
           'http://svn.pythonpaste.org/CmdUtils/trunk#egg=CmdUtils-dev',
           'http://svn.colorstudy.com/INITools/trunk#egg=INITools-dev',
           'http://svn.pythonpaste.org/Tempita/trunk#egg=Tempita-dev',
