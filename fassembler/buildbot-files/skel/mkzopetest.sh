f59f09343204fc7092bc508c8f76c6eb5bab4cad
Paul Winkler <slinkp@gmail.com>
2008-02-08 19:29:04 +0000

    fix paths for zope tests

--- fassembler/buildbot-files/skel/mkzopetest.sh
+++ fassembler/buildbot-files/skel/mkzopetest.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-ZOPETEST={{config.oc_basedir}}/zope/bin/zopetest
+ZOPETEST={{config.oc_basedir}}/opencore/zope/bin/zopetest
 
-sed 's/^ *exec.*$//g' {{config.oc_basedir}}/zope/bin/zopectl > $ZOPETEST
+sed 's/^ *exec.*$//g' {{config.oc_basedir}}/opencore/zope/bin/zopectl > $ZOPETEST
 echo '"$PYTHON" "$ZOPE_HOME"/bin/test.py -v --config-file "$CONFIG_FILE" "$@"' >> $ZOPETEST
 chmod u+x $ZOPETEST
 
