7a2a7abbc65be2f0c9b6bf58e103f9508370c87e
Paul Winkler <slinkp@gmail.com>
2008-04-07 19:37:32 +0000

    remove unused script

--- fassembler/buildbot-files/skel/mkzopetest.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-ZOPETEST={{config.oc_basedir}}/opencore/zope/bin/zopetest
-
-sed 's/^ *exec.*$//g' {{config.oc_basedir}}/opencore/zope/bin/zopectl > $ZOPETEST
-echo '"$PYTHON" "$ZOPE_HOME"/bin/test.py -v --config-file "$CONFIG_FILE" "$@"' >> $ZOPETEST
-chmod u+x $ZOPETEST
-
